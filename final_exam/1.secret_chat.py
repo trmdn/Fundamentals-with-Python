9a40f34dc8b4951690299c34bb54d631d88c3fb3
trmdn <doludunyao@gmail.com>
2023-11-29 10:39:20 +0200

    final_exam_exercises
    ex1 - the _imitation_game
    ex1 - secret_chat
    ex2 - emoji_detector

---
+++ final_exam/1.secret_chat.py
@@ -0,0 +1,28 @@
+main_string = input()
+command = input()
+
+while command != "Reveal":
+    command_type, *info = command.split(":|:")
+    found_error = False
+    if command_type == "ChangeAll":
+        substring, changed_index = info
+        main_string = main_string.replace(substring, changed_index)
+    elif command_type == "Reverse":
+        substring = info[0]
+        if substring not in main_string:
+            print("error")
+            found_error = True
+        else:
+            word_find = main_string.find(substring)
+            main_string = main_string[:word_find] + main_string[word_find: + len(substring)]
+            main_string += substring[::-1]
+    elif command_type == "InsertSpace":
+        index = int(info[0])
+        main_string = main_string[:index] + " " + main_string[index:]
+
+    if not found_error:
+        print(main_string)
+
+    command = input()
+
+print(f"You have a new text message: {main_string}")
